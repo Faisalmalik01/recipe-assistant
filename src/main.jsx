f18c26a7484973da67e8876fcc9216239f2003ac
Faisal <muhammadfaisalmalik01@gmail.com>
2025-07-25 11:20:14 +0530

    Update homepage and deploy scripts for GitHub Pages

--- src/main.jsx
+++ src/main.jsx
@@ -7,7 +7,7 @@ import App from "./App";
 import "./index.css";
 
 createRoot(document.getElementById('root')).render(
-  <BrowserRouter>
+  <BrowserRouter basename="/recipe-assistant">
     <ThemeProvider>            
     <React.StrictMode>
       <App />
